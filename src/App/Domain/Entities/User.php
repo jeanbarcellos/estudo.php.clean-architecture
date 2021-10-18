d735e630aca8dc75ec3630390720e010b2507a14
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-10-17 21:37:09 -0300

    Criação da entidade User

---
+++ src/App/Domain/Entities/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Domain\Entities;
+
+use Core\Domain\Entity;
+
+class User extends Entity
+{
+    protected string $name;
+    protected string $email;
+
+    public function __construct(string $name, string $email)
+    {
+        $this->name = $name;
+        $this->email = $email;
+    }
+
+    public function getname(): string
+    {
+        return $this->name;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+}
---
+++ src/Core/Domain/Entity.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Core\Domain;
+
+abstract class Entity
+{
+    protected $id;
+}
