79d8c18308ccea138680cabb06e848e9ffa8da56
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-10-18 09:41:30 -0300

    Melhorias

--- src/Core/Iterator/IOData.php
+++ src/Core/Iterator/IOData.php
@@ -2,21 +2,47 @@
 
 namespace Core\Iterator;
 
+use RuntimeException;
+
 abstract class IOData
 {
-    public $data;
+    private array $data;
 
     public function __construct(array $data = [])
     {
         $this->data = $data;
     }
 
-    public function getData()
+    public function getData(): array
     {
         return $this->data;
     }
 
-    public static function create(array $data = [])
+    public function hasValue(string $key): bool
+    {
+        return isset($this->data[$key]);
+    }
+
+    public function getValue(string $key)
+    {
+        return $this->data[$key];
+    }
+
+    public function exists(string $key): bool
+    {
+        return array_key_exists($key, $this->data);
+    }
+
+    public function __get(string $name)
+    {
+        if (!$this->exists($name)) {
+            throw new RuntimeException(sprintf("Undefined property: '%s'", $name));
+        }
+
+        return $this->getValue($name);
+    }
+
+    public static function create(array $data = []): self
     {
         return new static($data);
     }
