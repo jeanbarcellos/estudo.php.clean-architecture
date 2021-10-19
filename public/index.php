a36f26b2709892bfe32a20bb4413fabefb4cbb11
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-10-18 23:14:54 -0300

    Testes nos Boundary

--- public/index.php
+++ public/index.php
@@ -1,10 +1,40 @@
 <?php
 
 use App\Adapters\Database\UserRepository;
+use App\UseCases\UserCreateInputBoundary;
 use App\UseCases\UserCreateUseCase;
 use Core\UseCase\InputData;
 
 require __DIR__ . '/../bootstrap.php';
+
+$input = new UserCreateInputBoundary('Jean Barcellos', 'jeanbarcellos@hotmail.com');
+
+dump($input);
+
+// __call
+dump($input->getName());
+dump($input->getEmail());
+
+// __get
+dump($input->name);
+dump($input->email);
+
+// ArrayAccess
+dump($input['name']);
+dump($input['email']);
+
+// Factory
+$input = UserCreateInputBoundary::create([
+    'email' => 'jeanbarcellos@hotmail.com',
+    'name' => 'Jean Barcellos',
+]);
+
+dump($input);
+
+// Arrayable
+dump($input->toArray());
+
+exit;
 
 $userCase = new UserCreateUseCase(new UserRepository());
 
