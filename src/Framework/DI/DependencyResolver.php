62bb5e0ceb5506b2648093a693f9fed97b414e0b
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-10-21 18:31:00 -0300

    Framework/DI - Criação de um resolvedor de dependências

---
+++ src/Framework/DI/DependencyResolver.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Framework\DI;
+
+use Framework\DI\Container;
+use ReflectionMethod;
+use ReflectionParameter;
+use RuntimeException;
+
+class DependencyResolver
+{
+    /**
+     * @var \Framework\DI\Container
+     */
+    private $container;
+
+    public function __construct(Container $container)
+    {
+        $this->container = $container;
+    }
+
+    public function resolveClassMethodParameters(string $className, string $methodName, array $values = []): array
+    {
+        if (!method_exists($className, $methodName)) {
+            return $values;
+        }
+
+        $reflector = new ReflectionMethod($className, $methodName);
+
+        return $this->resolveParemeters($reflector->getParameters(), $values);
+    }
+
+    private function resolveParemeters(array $parameters, array $values = []): array
+    {
+        $results = [];
+        foreach ($parameters as $parameter) {
+            $results[$parameter->getName()] = $this->resolveParameter($parameter, $values);
+        }
+
+        return $results;
+    }
+
+    private function resolveParameter(ReflectionParameter $parameter, array $values = [])
+    {
+        $value = array_key_exists($parameter->getName(), $values) ? $values[$parameter->getName()] : null;
+
+        if (!is_null($parameter->getClass())) {
+            return $this->resolveParameterClass($parameter, $value);
+        }
+
+        return $this->resolveParameterPrimitive($parameter, $value);
+    }
+
+    private function resolveParameterClass(ReflectionParameter $parameter, $value = null)
+    {
+        return $this->container->get($parameter->getClass()->getName());
+    }
+
+    private function resolveParameterPrimitive(ReflectionParameter $parameter, $value = null)
+    {
+        if (!is_null($value)) {
+            if ($parameter->hasType()) {
+                settype($value, $parameter->getType()->getName());
+            }
+
+            return $value;
+        }
+
+        if ($parameter->isDefaultValueAvailable()) {
+            return $parameter->getDefaultValue();
+        }
+
+        throw new RuntimeException("Could not resolve parameter [$parameter] in class {$parameter->getDeclaringClass()->getName()}");
+    }
+}
