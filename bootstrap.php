ea48219b287ce45ffd6bcb4ee92087a9e7217d6b
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-10-21 19:25:29 -0300

    config - ajustes no bootstratp

--- bootstrap.php
+++ bootstrap.php
@@ -3,10 +3,13 @@
 use App\Adapters\Database\UserRepository;
 use App\Domain\Repositories\UserRepositoryInterface;
 use Framework\DI\Container;
+use Framework\DependencyResolver;
 
 require __DIR__ . '/vendor/autoload.php';
 
 # Binds do container
 $container = new Container();
+
+$container->singleton(DependencyResolver::class);
 
 $container->singleton(UserRepositoryInterface::class, UserRepository::class);
