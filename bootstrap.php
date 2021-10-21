c1bcb6d73d97203f5272bc6fe4ac9f8cd76997fa
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-10-21 00:32:06 -0300

    Adição da confguração do container no bootstrap do App

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,12 @@
 <?php
 
+use App\Adapters\Database\UserRepository;
+use App\Domain\Repositories\UserRepositoryInterface;
+use Framework\Container;
+
 require __DIR__ . '/vendor/autoload.php';
+
+# Binds do container
+$container = new Container();
+
+$container->singleton(UserRepositoryInterface::class, UserRepository::class);
