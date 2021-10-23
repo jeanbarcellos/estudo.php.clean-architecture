31debe10baaa793d7343327e7d7474f8ea82f2b5
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-10-23 16:26:05 -0300

    App\Adapters - Ajustes em Database

--- src/App/Adapters/Database/UserRepository.php
+++ src/App/Adapters/Database/UserRepository.php
@@ -9,12 +9,16 @@ class UserRepository implements UserRepositoryInterface
 {
     public function findAll(): array
     {
-        return [];
+        return [
+            new User('Aline Martins', 'aline.martins@hotnai.com'),
+            new User('Bruno cardoso', 'bruno.cardoso@hotnai.com'),
+            new User('Carlos eduardo santos', 'carlos.santos@hotnai.com'),
+        ];
     }
 
     public function findById(): ?User
     {
-        return null;
+        return new User('Aline Martins', 'aline.martins@hotnai.com');
     }
 
     public function insert(User $user): User
