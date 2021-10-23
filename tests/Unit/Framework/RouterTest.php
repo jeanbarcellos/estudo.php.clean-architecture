c287185684c8a1b9e196098f5f8cb59a21be24c7
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-10-23 17:20:40 -0300

    ajuste nos testes

--- tests/Unit/Framework/RouterTest.php
+++ tests/Unit/Framework/RouterTest.php
@@ -2,11 +2,33 @@
 
 namespace Tests\Unit\Framework;
 
+use Framework\DI\Container;
+use Framework\Http\Request;
 use Framework\Routing\Router;
 use PHPUnit\Framework\TestCase;
+use Framework\Http\Exceptions\NotFoundHttpException;
 
 class RouterTest extends TestCase
 {
+    private function getContainer(): Container
+    {
+        return Container::getInstance();
+    }
+
+    private function getRouter(): Router
+    {
+        return $this->getContainer()->get(Router::class);
+    }
+
+    private function createRequest($method, $path)
+    {
+        $server = $_SERVER;
+        $server['REQUEST_METHOD'] = $method;
+        $server['REQUEST_URI'] = $path;
+
+        return new Request($_GET, $_POST, [], $_COOKIE, $_FILES, $server);
+    }
+
     /**
      * Todo método de teste precisa iniciar com test e
      * descrever o que ele está testando, como
@@ -17,11 +39,13 @@ class RouterTest extends TestCase
     public function testEsseMetodoDescreveOQueDeveAcontecer()
     {
         // Arrange
-        $router = new Router('GET', '/ola-mundo');
+        $router = $this->getRouter();
         $expected = false;
 
+        $request = $this->createRequest('GET', 'ola-mundo');
+
         // Action
-        $actual = $router->handle();
+        $actual = $router->handle($request);
 
         // Assert
         $this->assertEquals($expected, $actual);
@@ -29,50 +53,42 @@ class RouterTest extends TestCase
 
     public function testVerificaSeEncontraRota()
     {
-        // instancio o Router informando o método HTTP e a url digitada
-        // estes são os dados que queremos encontrar
-        $router = new Router('GET', '/ola-mundo');
+        $method = 'GET';
+        $path = 'ola-mundo';
 
-        // registro minha primeira rota, esta rota só retorna um
-        // true como exemplo, ela poderia fazer qualquer outra coisa
-        $router->add('GET', '/ola-mundo', function () {
+        $router = $this->getRouter();
+
+        $router->add($method, $path, function () {
             return true;
         });
 
-        // executamos o método que encontra a rota atual
-        $result = $router->handle();
+        $request = $this->createRequest($method, $path);
 
-        // executo a ação da rota encontrada e pego o valor,
-        // note que estou executando o método que
-        // registrei quando usei o $router->add
-        $actual = $result();
+        // Act
+        $result = $router->handle($request);
+        $actual = $result->getContent();
 
-        // o valor que espero que seja retornado pelo $actual
-        $expected = true;
-
-        // verifico se o valor atual é o mesmo do esperado
-        $this->assertEquals($expected, $actual);
+        // Assert
+        $this->assertEquals(true, $actual);
     }
 
     public function testVerificaNaoSeEncontraRota()
     {
-        $router = new Router('GET', '/outra-url'); // esta rota não foi registrada
+        $this->expectException(NotFoundHttpException::class);
+
+        $method = 'GET';
+        $path = 'outra-mundo';
+
+        $router = $this->getRouter();
 
         // esta rota não é a que está sendo usada
-        $router->add('GET', '/ola-mundo', function () {
+        $router->add($method, $path, function () {
             return true;
         });
 
-        $result = $router->handle();
+        $request = $this->createRequest($method, 'outra-url');
 
-        $actual = $result;
-        $expected = false;
-
-        // estou usando o assertNotEquals
-        // que verifica se os valores são diferentes
-        // antes eu usei o assertEquals
-        // que verifica se eles são iguais
-        $this->assertEquals($expected, $actual);
+        $result = $router->handle($request);
     }
 
 }
